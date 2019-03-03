709a74b910222bfd4dd00030625dea9a7bf220a1
Moses Lee <moseslee90@gmail.com>
2019-03-03 09:09:23 -0800

    created click events that triggered card animation

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,7 @@
 console.log("Up and running!");
 var cards = [];
-var createCard = function(rank, suit, cardImage) {
+var cardsInPlay = [];
+var createCard = function (rank, suit, cardImage) {
 	var card = {
 		rank: rank,
 		suit: suit,
@@ -12,7 +13,27 @@ createCard("queen", "hearts", "images/queen-of-hearts.png");
 createCard("queen", "diamonds", "images/queen-of-diamonds.png");
 createCard("king", "diamonds", "images/king-of-hearts.png");
 createCard("king", "diamonds", "images/king-of-diamonds.png");
-var cardsInPlay = [];
+var flipCard = function (){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		console.log("2 Cards are in Play");
+		checkForMatch();
+		location.reload();
+	};
+};
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -20,14 +41,4 @@ var checkForMatch = function () {
 		alert("Sorry, try again.");
 	};
 };
-var flipCard = function (cardId){
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {
-		console.log("2 Cards are in Play");
-		checkForMatch();
-	};
-};
-flipCard(0);
-flipCard(2);
+createBoard();
